dc1a472da08070c9bbac5a2ba6064d3a88556427
unknown <mydelaraasturiano@gmail.com>
2017-05-01 23:33:19 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$my_name = "Mydel-Ar A. Asturiano";
+echo $my_name;
+echo "<br/>";
+
+$my_wife = "Jayzelle Anne T. Asturiano";
+echo $my_wife;
+echo "<br/>";
+
+$my_son = "Mydzell Arann T. Asturiano";
+echo $my_son;
+echo "<br/>";
+?>
